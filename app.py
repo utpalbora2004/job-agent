dc4b523e9663dddecfbfab1af6c68d715161d1e3
utpalbora2004 <utpalbora664@gmail.com>
2026-04-26 21:06:49 +0530

    make app continuous

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 import requests
+import time
 
 TOKEN = "8622743519: AAGErMlJBxTpuzdseFgEhX5dYZXDNbAgrM0"
 CHAT_ID = "7289211505"
 
 def send_message(text):
-    url = f"https://api.telegram.org/bot8622743519:AAGErMlJBxTpuzdseFgEhX5dYZXDNbAgrM0/sendMessage"
-    data = {
-        "chat_id": 7289211505,
-        "text": text
-    }
-    respond=requests.post(url, data=data)
-    print(respond.text)
+    url = f"https://api.telegram.org/bot8622743519: AAGErMlJBxTpuzdseFgEhX5dYZXDNbAgrM0/sendMessage"
+    data = {"chat_id": 7289211505, "text": text}
+    requests.post(url, data=data)
 
 if __name__ == "__main__":
-    send_message(" Your Job Agent is Working!")
+    while True:
+        send_message("🚀 Job Agent is Running...")
+        time.sleep(3600)  # every 1 hour
