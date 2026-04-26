dd9de614f963819aaf0c2621b64d8da1941d2c51
utpalbora2004 <utpalbora664@gmail.com>
2026-04-26 21:22:47 +0530

    fix token usage

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ TOKEN = "8622743519: AAGErMlJBxTpuzdseFgEhX5dYZXDNbAgrM0"
 CHAT_ID = "7289211505"
 
 def send_message(text):
-    url = f"https://api.telegram.org/bot8622743519: AAGErMlJBxTpuzdseFgEhX5dYZXDNbAgrM0/sendMessage"
-    data = {"chat_id": 7289211505, "text": text}
+    url = f"https://api.telegram.org/bot8622743519:AAGErMlJBxTpuzdseFgEhX5dYZXDNbAgrM0/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": text}
     requests.post(url, data=data)
 
 if __name__ == "__main__":
